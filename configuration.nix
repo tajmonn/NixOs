a2ff3b86e31aacec594966b65c8ae3585c9969c5
tajmonn <fuatita@gmail.com>
2025-07-30 11:34:40 +0200

    gh

--- configuration.nix
+++ configuration.nix
@@ -102,6 +102,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    gh
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
