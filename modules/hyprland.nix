517c3fa2ec37ebe298f66a2e1cf2a6d7f79bfe13
tajmonn <fuatita@gmail.com>
2025-08-02 11:33:17 +0200

    enabled hyprland

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -10,11 +10,18 @@
     # Enable X11 server support (needed for Hyprland and fallback & XWayland)
     services.xserver.enable = true;
 
+    # From wiki
+    programs.hyprland.enable = true;
+    environment.systemPackages = [
+        pkgs.kitty # required for the default Hyprland config
+    ];
+
     # Basic Hyprland config deployed system-wide
     environment.etc."hyprland.conf".text = ''
         monitor=eDP-1,2256x1506@60,0x0,1
         # TODO add custom binds and settings
     '';
+
 
 
 }
